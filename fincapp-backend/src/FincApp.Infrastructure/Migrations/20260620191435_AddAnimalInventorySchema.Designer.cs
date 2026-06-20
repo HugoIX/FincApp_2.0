dc675b9a50a7ed0ad56ec937fb1d90f8953c7a06
Sergio Alejandro Ospina Tabares <saot-31@hotmail.com>
2026-06-20 15:10:38 -0500

    chore(database): generate animal schema migrations

--- fincapp-backend/src/FincApp.Infrastructure/Migrations/20260620191435_AddAnimalInventorySchema.Designer.cs
+++ fincapp-backend/src/FincApp.Infrastructure/Migrations/20260620191435_AddAnimalInventorySchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FincApp.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FincApp.Infrastructure.Migrations
 {
     [DbContext(typeof(FincAppDbContext))]
-    partial class FincAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260620191435_AddAnimalInventorySchema")]
+    partial class AddAnimalInventorySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
